4a24d0dbf1b72f93013a0b1910ec33618d39e955
SRIDHARANITHARAN.B <username420sri@gmail.com>
2024-07-30 14:39:50 -0700

    three game's are available is Now

--- game.c
+++ game.c
@@ -7,10 +7,32 @@
 #include<sys/types.h>
 #include "user_details.h"
 #include "games.h"
+#include "print_data.h"
 
 
 
-int main(){
+
+int main(int argc ,char *argv[]){
+    int choice;
+    banner_two();
+    printf("Enter your choice \n");
+    switch (scanf("%d",choice))
+    {
+    case 1:
+        pick_a_correct_number();
+        break;
+    case 2:
+        no_match_dealer();
+        break;
+    case 3:
+        find_ace();
+        break;
+    default:
+        // printf("Invalid choice , Use help comment %s --help or -h",argv[0]);
+        printf("Choice is invalide");
+        banner_two();
+        break;
+    }
 //     if(check_uid_is_live() == -1){
 //         new_register();
 //     }
---
+++ print_data.h
@@ -0,0 +1,10 @@
+#ifndef PRINT_DATA_H
+#define PRINT_DATA_H
+// void banner_one(){
+//     printf("Written by");
+// }
+void banner_two(){
+    printf("1.Pick  a correct Number\n2. No Match dealer \n3. Find Ace Game");
+    
+}
+#endif
