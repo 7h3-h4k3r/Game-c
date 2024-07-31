929c8083911538585d66b9fb6f3c96bd92b230fe
SRIDHARANITHARAN.B <username420sri@gmail.com>
2024-07-31 09:06:07 -0700

    Update game.c

--- game.c
+++ game.c
@@ -10,7 +10,10 @@
 #include "print_data.h"
 
 int main(int argc ,char *argv[]){
-    int choice;
+     int choice;
+    if(check_uid_is_live == -1){
+        new_register();
+    }
     banner_one();
     banner_two();
     printf("Enter your choice \n");
